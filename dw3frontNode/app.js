5c29b484cd429de7494e9bd4dd55c0eebd2c38a8
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2023-09-20 10:41:26 -0300

    err fix

--- dw3frontNode/app.js
+++ dw3frontNode/app.js
@@ -1,41 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//app.js
+ // DEBUG=dw3frontnode:* npm start
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+ var createError = require('http-errors');
+ var express = require('express');
+ var path = require('path');
+ var cookieParser = require('cookie-parser');
+ var logger = require('morgan');
 
-var app = express();
+ const session = require('express-session');
+ require('dotenv').config();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'vash');
+ var indexRouter = require('./routes/index');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+ var app = express();
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+ // view engine setup
+ app.set('views', path.join(__dirname, 'views'));
+ 
+ app.set('view engine', 'vash');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+ app.use(logger('dev'));
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: false }));
+ app.use(cookieParser());
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+ app.use(
+ session({
+ secret: "palavrasecreta",
+ resave: false,
+ saveUninitialized: true,
+ cookie: { maxAge: null },
+ })
+ );
 
-module.exports = app;
+ app.use(express.static(path.join(__dirname, 'public')));
+
+ app.use('/', indexRouter);
+
+ // catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+ next(createError(404));
+ });
+
+ // error handler
+ app.use(function(err, req, res, next) {
+ // set locals, only providing error in development
+ res.locals.message = err.message;
+ res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+ // render the error page
+ res.status(err.status || 500);
+ res.render('error');
+ });
+
+ module.exports = app;
